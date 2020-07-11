2f41036d07e0ff8727df1274ecb2e6062411e46b
Blaise Thompson <blaise@untzag.com>
2020-07-11 13:55:17 -0500

    refactor

---
+++ ophyd_yaq/_exceptions.py
@@ -0,0 +1,44 @@
+__all__ = ["YaqcBlueskyException",
+           "InvalidState",
+           "UnknownStatusFailure",
+           "StatusTimeoutError",
+           "WaitTimeoutError",
+           ]
+
+
+class YaqcBlueskyException(Exception):
+    """
+    yaqc-bluesky base exception class
+    """
+    pass
+
+
+class InvalidState(RuntimeError, YaqcBlueskyException):
+    """
+    When Status.set_finished() or Status.set_exception(exc) is called too late
+    """
+    ...
+
+
+class UnknownStatusFailure(YaqcBlueskyException):
+    """
+    Generic error when a Status object is marked success=False without details.
+    """
+    ...
+
+
+class StatusTimeoutError(TimeoutError, YaqcBlueskyException):
+    """
+    Timeout specified when a Status object was created has expired.
+    """
+    ...
+
+
+class WaitTimeoutError(TimeoutError, YaqcBlueskyException):
+    """
+    TimeoutError raised when we ware waiting on completion of a task.
+    This is distinct from TimeoutError, just as concurrent.futures.TimeoutError
+    is distinct from TimeoutError, to differentiate when the task itself has
+    raised a TimeoutError.
+    """
+    ...
--- ophyd_yaq/_hardware.py
+++ ophyd_yaq/_hardware.py
@@ -1,38 +1,32 @@
-from ophyd import Component as Cpt, Signal, DeviceStatus
+from collections import OrderedDict
 import threading
 import time
 
 from ._base import Base
+from ._status import Status
 
 
 class Hardware(Base):
-    setpoint = Cpt(Signal, kind="hinted")
-    readback = Cpt(Signal)
+
+    def _read(self, out, ts) -> OrderedDict:
+        out = super()._read(out, ts)
+        out["setpoint"] = {"value" : self.yaq_client.get_destination(), "timestamp" : ts}
+        out["readback"] = {"value" : self.yaq_client.get_position(), "timestamp" : ts}
+        return out
 
     def set(self, value):
-        with self._busy_lock:
-            self.busy.put(1)
+        print(value)
         self.yaq_client.set_position(value)
-        st = DeviceStatus(self)
+        st = Status()
 
         def poll_busy():
             while True:
-                self._read_yaq()
-                if self.yaq_client.busy():
+                r = self.read()
+                if r["busy"]["value"]:
                     time.sleep(0.1)
                 else:
                     break
-            with self._busy_lock:
-                self.busy.put(False)
             st._finished()
 
         threading.Thread(target=poll_busy).start()
-        # update the signals
         return st
-
-    def _read_yaq(self):
-        self.setpoint.put(self.yaq_client.get_destination())
-        self.readback.put(self.yaq_client.get_position())
-        b = self.yaq_client.busy()
-        with self._busy_lock:
-            self.busy.put(int(b))
