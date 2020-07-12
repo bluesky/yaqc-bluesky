24a03c29141bcd05c50156cfd9ecfaa5fb8b6a8d
Blaise Thompson <blaise@untzag.com>
2020-07-11 22:16:04 -0500

    sensor

--- binder/launch-daemons.sh
+++ binder/launch-daemons.sh
@@ -2,4 +2,5 @@ yaqd-fake-continuous-hardware --config ${REPO_DIR}/binder/fake-continuous-hardwa
 python -c "import yaqc; c = yaqc.Client(38100); c.set_position(0)"
 python -c "import yaqc; c = yaqc.Client(38101); c.set_position(0)"
 python -c "import yaqc; c = yaqc.Client(38102); c.set_position(0)"
+yaqd-fake-triggered-sensor --config ${REPO_DIR}/binder/fake-triggered-sensor-config.toml &>/dev/null &
 disown
