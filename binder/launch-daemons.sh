5f0b504c12ccfe1b3f198c571aff0682bf7d2cfa
Blaise Thompson <blaise@untzag.com>
2020-07-11 21:32:08 -0500

    index

--- binder/launch-daemons.sh
+++ binder/launch-daemons.sh
@@ -1,2 +1,5 @@
-yaqd-fake-continuous-hardware --config ${REPO_DIR}/binder/fake-continuous-hardware-config.toml %>/dev/null &
+yaqd-fake-continuous-hardware --config ${REPO_DIR}/binder/fake-continuous-hardware-config.toml &>/dev/null &
+python -c "import yaqc; c = yaqc.Client(38100); c.set_position(0)"
+python -c "import yaqc; c = yaqc.Client(38101); c.set_position(0)"
+python -c "import yaqc; c = yaqc.Client(38102); c.set_position(0)"
 disown
