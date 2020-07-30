2f6c31d3775b3d80c6dc43404f67c4c4061b86e7
Blaise Thompson <blaise@untzag.com>
2020-07-30 17:38:07 -0500

    pyproject.toml

--- yaqc_bluesky/__init__.py
+++ yaqc_bluesky/__init__.py
@@ -1,8 +1,3 @@
-# flake8: noqa
-
 from ._device import *
 from ._exceptions import *
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from .__version__ import *
