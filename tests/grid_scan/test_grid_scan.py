80dcd0523b9a95a7c880075c5299b71e0f7b276e
Blaise Thompson <blaise@untzag.com>
2020-08-26 20:15:35 -0500

    grid_scan

---
+++ tests/grid_scan/test_grid_scan.py
@@ -0,0 +1,27 @@
+import pathlib
+import time
+import subprocess
+import yaqc_bluesky
+from yaqd_core import testing
+from bluesky import RunEngine
+from bluesky.plans import grid_scan
+
+
+__here__ = pathlib.Path(__file__).parent
+
+
+@testing.run_daemon_entry_point(
+    "fake-triggered-sensor", config=__here__ / "triggered-sensor-config.toml"
+)
+@testing.run_daemon_entry_point(
+    "fake-continuous-hardware", config=__here__ / "continuous-hardware-config.toml"
+)
+def test_simple_grid_scan():
+    RE = RunEngine()
+    hardware = yaqc_bluesky.Device(39424)
+    sensor = yaqc_bluesky.Device(39425)
+    RE(grid_scan([sensor], hardware, -10, 10, 15))
+
+
+if __name__ == "__main__":
+    test_simple_grid_scan()
