5cd02903f4fa069a17021ffb676d32e7a4c920a0
Blaise Thompson <blaise@untzag.com>
2020-08-26 20:37:40 -0500

    log_scan

---
+++ tests/log_scan/test_log_scan.py
@@ -0,0 +1,27 @@
+import pathlib
+import time
+import subprocess
+import yaqc_bluesky
+from yaqd_core import testing
+from bluesky import RunEngine
+from bluesky.plans import log_scan
+
+
+__here__ = pathlib.Path(__file__).parent
+
+
+@testing.run_daemon_entry_point(
+    "fake-triggered-sensor", config=__here__ / "triggered-sensor-config.toml"
+)
+@testing.run_daemon_entry_point(
+    "fake-continuous-hardware", config=__here__ / "continuous-hardware-config.toml"
+)
+def test_simple_log_scan():
+    RE = RunEngine()
+    hardware = yaqc_bluesky.Device(39424)
+    sensor = yaqc_bluesky.Device(39425)
+    RE(log_scan([sensor], hardware, -10, 10, 15))
+
+
+if __name__ == "__main__":
+    test_simple_log_scan()
