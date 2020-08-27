3c0484fd1253c30321cc47ceb0c0967108bfac17
Blaise Thompson <blaise@untzag.com>
2020-08-26 19:51:25 -0500

    rel_scan

--- tests/rel_scan/test_rel_scan.py
+++ tests/rel_scan/test_rel_scan.py
@@ -4,7 +4,7 @@ import subprocess
 import yaqc_bluesky
 from yaqd_core import testing
 from bluesky import RunEngine
-from bluesky.plans import count
+from bluesky.plans import rel_scan
 
 
 __here__ = pathlib.Path(__file__).parent
@@ -13,12 +13,16 @@ __here__ = pathlib.Path(__file__).parent
 @testing.run_daemon_entry_point(
     "fake-triggered-sensor", config=__here__ / "triggered-sensor-config.toml"
 )
-def test_simple_count():
+@testing.run_daemon_entry_point(
+    "fake-continuous-hardware", config=__here__ / "continuous-hardware-config.toml"
+)
+def test_simple_rel_scan():
     RE = RunEngine()
     hardware = yaqc_bluesky.Device(39424)
+    print(hardware.stop)
     sensor = yaqc_bluesky.Device(39425)
-    RE(count([sensor], 41))
+    RE(rel_scan([sensor], hardware, -1, 1, 15))
 
 
 if __name__ == "__main__":
-    test_simple_count()
+    test_simple_rel_scan()
