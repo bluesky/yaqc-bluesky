addf38950b834c31044b78825d656e1087eb848c
Blaise Thompson <blaise@untzag.com>
2021-01-24 13:30:43 -0600

    overload happi name regex

--- yaqc_bluesky/_happi.py
+++ yaqc_bluesky/_happi.py
@@ -1,14 +1,20 @@
-"""Support for happi - 
-pcdshub.github.io/happi 
+"""Support for happi -
+pcdshub.github.io/happi
 """
 
 
+import re
 import copy
 
 from happi.item import HappiItem, EntryInfo  # type: ignore
 
 
 class YAQItem(HappiItem):
+    name = EntryInfo(
+        "Shorthand Python-valid name for the Python instance",
+        optional=False,
+        enforce=re.compile(r"^[^\d\W]\w*\Z"),
+    )  # https://stackoverflow.com/a/10134719
     port = EntryInfo("TCP port.", enforce=int, optional=False)
     host = EntryInfo("Host.", optional=True, default="localhost")
     kwargs = copy.copy(HappiItem.kwargs)
