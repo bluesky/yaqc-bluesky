c5d7ee37358e2f52f0600d1344e9302c635339ce
Kyle Sunden <git@ksunden.space>
2022-01-12 01:22:57 -0600

    Initial implementation of depenendent hardware

---
+++ yaqc_bluesky/_has_dependent.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+import time
+import warnings
+
+from ._base import Base
+
+
+class HasDependent(Base):
+    def __init__(self, yaq_client, *, name=None):
+        super().__init__(yaq_client, name=name)
+        self._dependent_hardware = {}
+        for k, v in self.yaq_client.get_dependent_hardware().items():
+            try:
+                dev = Device(port=int(v.split(":", 1)[1]), host=v.split(":", 1)[0])
+                self._dependent_hardware[k] = dev
+            except:
+                warnings.warn(
+                    f"Unable to connect to {k} from {self.name}, ignoring dependent relationship."
+                )
+
+    def _describe(self, out):
+        out = super()._describe(out)
+        for d in self._dependent_hardware.values():
+            out.update(d.describe())
+        return out
+
+    def _read(self, out, ts) -> OrderedDict:
+        out = super()._read(out, ts)
+        for d in self._dependent_hardware.values():
+            out.update(d.read())
+        return out
+
+    def read_configuration(self) -> OrderedDict:
+        out = super().read_configuration()
+        for d in self._dependent_hardware.values():
+            out.update(d.read_configuration())
+        return out
+
+    def describe_configuration(self) -> OrderedDict:
+        out = super().describe_configuration()
+        for d in self._dependent_hardware.values():
+            out.update(d.describe_configuration())
+        return out
