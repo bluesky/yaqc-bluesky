9f0c384a5eb37b7450cce7a6a739360d2286523d
Kyle Sunden <git@ksunden.space>
2022-01-14 16:36:17 -0600

    hints, public variable

--- yaqc_bluesky/_has_dependent.py
+++ yaqc_bluesky/_has_dependent.py
@@ -11,14 +11,14 @@ class HasDependent(Base):
         # Avoid circular import
         from ._device import Device
 
-        self._dependent_hardware = {}
+        self.dependent_hardware = {}
         for k, v in self.yaq_client.get_dependent_hardware().items():
             try:
                 host, port = v.split(":", 1)
                 # replace hosts local to my own daemon, which may be remote to the client
                 if host in ("localhost", "127.0.0.1"):
                     host = self.yaq_client._host
-                self._dependent_hardware[k] = Device(port=port, host=host)
+                self.dependent_hardware[k] = Device(port=port, host=host)
             except ConnectionError as e:
                 warnings.warn(
                     f"Unable to connect to {k} from {self.name}, ignoring dependent relationship."
@@ -26,24 +26,30 @@ class HasDependent(Base):
 
     def _describe(self, out):
         out = super()._describe(out)
-        for d in self._dependent_hardware.values():
+        for d in self.dependent_hardware.values():
             out.update(d.describe())
         return out
 
     def _read(self, out, ts) -> OrderedDict:
         out = super()._read(out, ts)
-        for d in self._dependent_hardware.values():
+        for d in self.dependent_hardware.values():
             out.update(d.read())
         return out
 
     def read_configuration(self) -> OrderedDict:
         out = super().read_configuration()
-        for d in self._dependent_hardware.values():
+        for d in self.dependent_hardware.values():
             out.update(d.read_configuration())
         return out
 
     def describe_configuration(self) -> OrderedDict:
         out = super().describe_configuration()
-        for d in self._dependent_hardware.values():
+        for d in self.dependent_hardware.values():
             out.update(d.describe_configuration())
+        return out
+
+    @property
+    def hints(self):
+        out = super().hints
+        out["yaq dependents"] = list(self.dependent_hardware.keys())
         return out
