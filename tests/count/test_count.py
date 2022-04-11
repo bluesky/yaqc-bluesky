9e231c1c2b7c6002dbe7307400e58019fa6706cb
Kyle Sunden <git@ksunden.space>
2022-04-11 14:45:00 -0500

    Fixes for cameras and mappings (#77)
    * Fixes for cameras and mappings
    
    Technically yaq mappings are even more flexible than this handling accounts for, however I know of no real daemons which are caught by that so meh, cross that bridge when we come to it
    
    Includes tests including getting the xarray out of count, as that is where it actually failed prior to this change
    
    * import databroker.v2...
    
    * pin databroker since we know the new version is quite breaking

--- tests/count/test_count.py
+++ tests/count/test_count.py
@@ -5,6 +5,7 @@ import yaqc_bluesky
 from yaqd_core import testing
 from bluesky import RunEngine
 from bluesky.plans import count
+import databroker.v2
 
 
 __here__ = pathlib.Path(__file__).parent
@@ -17,6 +18,16 @@ def test_simple_count():
     RE = RunEngine()
     sensor = yaqc_bluesky.Device(39425)
     RE(count([sensor], 41))
+
+
+@testing.run_daemon_entry_point("fake-camera", config=__here__ / "camera-config.toml")
+def test_camera_count():
+    cat = databroker.v2.temp()
+    RE = RunEngine()
+    RE.subscribe(cat.v1.insert)
+    sensor = yaqc_bluesky.Device(39425)
+    RE(count([sensor], 4))
+    cat[-1].primary.read()
 
 
 if __name__ == "__main__":
