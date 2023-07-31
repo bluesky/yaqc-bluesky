19e25c357f4b1196ad57641911453079811e4c34
Blaise Thompson <blaise@untzag.com>
2023-07-31 11:59:11 -0500

    not get, has

--- yaqc_bluesky/_base.py
+++ yaqc_bluesky/_base.py
@@ -28,7 +28,7 @@ class Base:
             if prop.type not in ["double"]:
                 continue
             self.children.append(PropertyDevice(self, key))
-            if not getattr(self, key):  # don't overwrite please
+            if not hasattr(self, key):  # don't overwrite please
                 setattr(self, key, self.children[-1])
 
     def __repr__(self):
